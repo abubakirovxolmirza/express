efe6a83ed935935248de7fad57902efe1c29727e
abubakirovxolmirza <137431926+abubakirovxolmirza@users.noreply.github.com>
2024-04-08 21:36:25 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-nt_tun=@3mm&fau$^+x2_4^t)fwbbi50ct8p7i)cejv-*)0xco
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['164.92.253.252:8000']
+ALLOWED_HOSTS = ['example.com', 'www.example.com', '164.92.253.252']
+
 
 
 # Application definition
