16b3966d42d8150c72a87ac8cdec09c154eba4fb
abubakirovxolmirza <137431926+abubakirovxolmirza@users.noreply.github.com>
2024-04-09 00:14:21 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('express.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/swagger', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
