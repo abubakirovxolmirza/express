7f8b4a988c61d58016d6292b5c05e45169f4943b
abubakirovxolmirza <abubakirovxolmirza1@gmail.com>
2024-04-15 10:44:48 +0500

    Bug Fixed

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nt_tun=@3mm&fau$^+x2_4^t)fwbbi50ct8p7i)cejv-*)0xco
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['64.226.120.39', '8000']
+ALLOWED_HOSTS = ['64.226.120.39']
 
 
 # Application definition
